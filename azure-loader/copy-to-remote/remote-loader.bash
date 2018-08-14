6fb741701d0eaa6fafa27b49069938bf70db631e
Daniel Voros <dvoros@hortonworks.com>
2018-08-14 12:05:00 +0200

    moved data from S3

--- azure-loader/copy-to-remote/remote-loader.bash
+++ azure-loader/copy-to-remote/remote-loader.bash
@@ -5,15 +5,15 @@ DATA_DIR=/tmp/loader/data
 # Download data
 mkdir $DATA_DIR
 cd $DATA_DIR
-wget -q https://s3.eu-central-1.amazonaws.com/hive-workshop/tpcds_1g.tar.gz
+wget -q https://asdasd.hu/hive-workshop/tpcds_1g.tar.gz
 tar xzf tpcds_1g.tar.gz
-wget -q https://s3.eu-central-1.amazonaws.com/hive-workshop/police_data.tar.gz
+wget -q https://asdasd.hu/hive-workshop/police_data.tar.gz
 tar xzf police_data.tar.gz
-wget -q https://s3.eu-central-1.amazonaws.com/hive-workshop/discogs_20170801_releases_text.tar.gz
+wget -q https://asdasd.hu/hive-workshop/discogs_20170801_releases_text.tar.gz
 tar xzf discogs_20170801_releases_text.tar.gz
-wget -q https://s3.eu-central-1.amazonaws.com/hive-workshop/discogs_20170801_artists_text.tar.gz
+wget -q https://asdasd.hu/hive-workshop/discogs_20170801_artists_text.tar.gz
 tar xzf discogs_20170801_artists_text.tar.gz
-wget -q https://s3.eu-central-1.amazonaws.com/hive-workshop/discogs_20170801_labels_text.tar.gz
+wget -q https://asdasd.hu/hive-workshop/discogs_20170801_labels_text.tar.gz
 tar xzf discogs_20170801_labels_text.tar.gz
 rm *.tar.gz
 
